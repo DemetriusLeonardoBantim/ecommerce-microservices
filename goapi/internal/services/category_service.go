abd097d47c18effc23d89711f0e814a05c123f60
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2025-07-21 20:41:00 -0300

    feat: create service method to category

--- goapi/internal/services/category_service.go
+++ goapi/internal/services/category_service.go
@@ -1,6 +1,9 @@
 package services
 
-import "github.com/DemetriusLeonardoBantim/ecommerce-microservices/goapi/internal/database"
+import (
+	"github.com/DemetriusLeonardoBantim/ecommerce-microservices/goapi/internal/database"
+	"github.com/DemetriusLeonardoBantim/ecommerce-microservices/goapi/internal/entity"
+)
 
 type CategoryService struct {
 	CategoryDB database.CategoryDB
@@ -8,4 +11,29 @@ type CategoryService struct {
 
 func NewCategoryService(categoryDB database.CategoryDB) *CategoryService {
 	return &CategoryService{CategoryDB: categoryDB}
+}
+
+func (cs *CategoryService) GetCategories() ([]*entity.Category, error) {
+	categories, err := cs.CategoryDB.GetCategories()
+	if err != nil {
+		return nil, err
+	}
+	return categories, nil
+}
+
+func (cs *CategoryService) CreateCategory(name string) (*entity.Category, error) {
+	category := entity.NewCategory(name)
+	_, err := cs.CategoryDB.CreateCategory(category)
+	if err != nil {
+		return nil, err
+	}
+	return category, nil
+}
+
+func (cs *CategoryService) GetCategory(id string) (*entity.Category, error) {
+	category, err := cs.CategoryDB.GetCategory(id)
+	if err != nil {
+		return nil, err
+	}
+	return category, nil
 }
