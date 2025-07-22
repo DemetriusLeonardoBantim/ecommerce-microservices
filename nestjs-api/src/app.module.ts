c68551219f50fc7371c398c95dccd16f34eaf96a
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2025-07-22 13:53:51 -0300

    refactor: app module

--- nestjs-api/src/app.module.ts
+++ nestjs-api/src/app.module.ts
@@ -4,19 +4,24 @@ import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product } from './products/entities/product.entity';
+import { OrdersModule } from './orders/orders.module';
+import { Order } from './orders/entities/order.entity';
 
 @Module({
   imports: [
     ProductsModule,
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'locahost',
+      host: 'localhost',
       port: 3306,
       username: 'root',
       password: 'root',
-      database: 'root',
-      entities: [Product],
+      database: 'nest',
+      entities: [Product, Order],
+      synchronize: true,
+      logging: true,
     }),
+    OrdersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
