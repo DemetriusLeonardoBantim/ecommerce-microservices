c5fd1e140f0d60544c22f7834d8ae32c87cc11b6
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2025-07-22 08:35:56 -0300

    chore: configure typeorm module  in app module

--- nestjs-api/src/app.module.ts
+++ nestjs-api/src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Product } from './products/entities/product.entity';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [
+    ProductsModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'locahost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'root',
+      entities: [Product],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
